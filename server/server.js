0760911f16ca221bf6fd0c257f971f0f53ca3f1d
Yoong1117 <jiangyoong1117@gmail.com>
2025-04-19 15:26:47 +0800

    localhost to Render

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.MONGODB_URL).then(() => {
   console.log("Connected to MongoDB Atlas");
 }).catch((err) => {
   console.error("MongoDB connection error:", err);
